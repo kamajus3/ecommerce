e5dbb2eb34826f53abde8f7a3cda09fd73d79dcc
Carlinhos <91576261+carl0smat3us@users.noreply.github.com>
2024-02-22 13:56:22 +0100

    feat: Update Admin Header

--- src/app/components/Header/AdminHeader.tsx
+++ src/app/components/Header/AdminHeader.tsx
@@ -8,13 +8,13 @@ export default function AdminHeader() {
         <h1 className="text-black font-bold text-2xl">Racius Care</h1>
 
         <div className="flex gap-4 items-center mr-4">
-          <Link href="#" className="font-medium text-black">
+          <Link href="/admin/analytics" className="font-medium text-black max-sm:hidden">
             Analytics
           </Link>
-          <Link href="#" className="font-medium text-black">
-            Histório
+          <Link href="/admin/sells" className="font-medium text-black max-sm:hidden">
+            Vendas
           </Link>
-          <Link href="/admin/products" className="font-medium text-black">
+          <Link href="/admin/products" className="font-medium text-black max-sm:hidden">
             Productos
           </Link>
           <div className="h-11 flex gap-4 items-center justify-between">
