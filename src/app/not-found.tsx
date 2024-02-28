b5f1b5441c151a18e5df94aae71b31a25f4cf877
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-02-28 18:39:21 +0100

    feat: Add custom not found error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,26 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <section className="w-screen h-screen">
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-main">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Página não encontrada
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Desculpe, não conseguimos encontrar oque procuras.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="bg-main px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:brightness-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Voltar para o início
+            </Link>
+          </div>
+        </div>
+      </main>
+    </section>
   )
 }
