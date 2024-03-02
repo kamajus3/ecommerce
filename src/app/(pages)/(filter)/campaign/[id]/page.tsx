8aadf3ba3a21ff3b799aae7487216f4c3a583862
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-03-02 18:48:49 +0100

    feat: Improve campaign SEO

--- src/app/(pages)/(filter)/campaign/[id]/page.tsx
+++ src/app/(pages)/(filter)/campaign/[id]/page.tsx
@@ -1,123 +1,42 @@
-'use client'
-
-import Header from '@/app/components/Header'
-import ProductCard from '@/app/components/ProductCard'
-import Footer from '@/app/components/Footer'
-import { notFound, useParams } from 'next/navigation'
-import { Suspense, useEffect, useState } from 'react'
-import { ProductItem, PromotionItemBase } from '@/@types'
-import { getProducts } from '@/lib/firebase/database'
-import clsx from 'clsx'
-import { SearchX } from 'lucide-react'
+import { Suspense } from 'react'
+import { SearchPageWithoutBoundary } from './campaign'
 import Loading from '@/app/components/Loading'
+import { Metadata } from 'next'
 import { child, get, ref } from 'firebase/database'
 import { database } from '@/lib/firebase/config'
-import { campaignValidator, publishedSince } from '@/functions'
+import { campaignValidator } from '@/functions'
 
-function SearchPageWithoutBoundary() {
-  const [productData, setProductData] = useState<Record<string, ProductItem>>(
-    {},
-  )
-  const [promotionData, setPromotionData] = useState<PromotionItemBase>()
-  const [loading, setLoading] = useState(true)
-  const { id: campaign } = useParams<{ id: string }>()
-
-  useEffect(() => {
-    async function unsubscribed() {
-      get(child(ref(database), `promotions/${campaign}`)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setPromotionData(snapshot.val())
-        } else {
-          notFound()
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string }
+}): Promise<Metadata> {
+  return new Promise((resolve) => {
+    get(child(ref(database), `promotions/${params.id}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        if (!campaignValidator(snapshot.val())) {
+          resolve({
+            title: 'Campanha não encontrada.',
+          })
         }
-      })
-
-      await getProducts({
-        promotion: campaign,
-      }).then((products) => {
-        setProductData(products)
-      })
-
-      setLoading(false)
-    }
-
-    unsubscribed()
-  }, [campaign])
-
-  const resultsCount = Object.keys(productData).length
-
-  return (
-    <section className="bg-white min-h-screen overflow-hidden">
-      <Header.Client />
-
-      {promotionData && (
-        <article className="p-4 mx-auto mt-9">
-          {campaignValidator(promotionData) === 'promotion' && (
-            <span className="text-black font-medium text-sm uppercase">
-              A promoção termina {publishedSince(promotionData.finishDate)}
-            </span>
-          )}
-          <h2 className="text-black font-semibold text-2xl">
-            {promotionData.title}
-          </h2>
-          <p className="text-[#212121] text-sm max-w-10">
-            {promotionData.description}
-          </p>
-        </article>
-      )}
-
-      <p
-        className={clsx(
-          'text-black font-semibold text-3xl p-4 mb-4 max-sm:text-center',
-          {
-            hidden: resultsCount === 0,
+        resolve({
+          title: snapshot.val().title,
+          description: snapshot.val().description.slice(0, 100) + '...',
+          openGraph: {
+            type: 'article',
+            title: snapshot.val().title,
+            description: snapshot.val().description,
+            siteName: 'Racius Care',
+            images: snapshot.val().photo,
           },
-        )}
-      >
-        {resultsCount < 100
-          ? `Foram encontrados ${resultsCount} resultado(s).`
-          : `Mais de ${resultsCount} resultados encontrados`}
-      </p>
-
-      <div
-        className={clsx(
-          'w-screen min-h-[120vh] flex flex-wrap gap-9 p-6 justify-center mb-8 max-w-[98vw]',
-          {
-            hidden: resultsCount === 0,
-          },
-        )}
-      >
-        {Object.entries(productData).map(([id, product]) => (
-          <ProductCard key={id} {...product} id={id} />
-        ))}
-      </div>
-
-      <div
-        className={clsx(
-          'w-screen min-h-[60vh] flex flex-col items-center gap-6 p-4 justify-center mb-8',
-          {
-            hidden: resultsCount !== 0 || loading,
-          },
-        )}
-      >
-        <SearchX size={60} color="#000" />
-        <p className="text-black font-semibold text-xl">
-          Nenhum resultado encontrado
-        </p>
-      </div>
-
-      {loading && (
-        <div className="w-screen min-h-[60vh] flex flex-col items-center gap-6 p-4 justify-center mb-8">
-          <div
-            className="inline-block h-8 w-8 animate-spin rounded-full border-main border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          />
-        </div>
-      )}
-
-      <Footer />
-    </section>
-  )
+        })
+      } else {
+        resolve({
+          title: 'Campanha não encontrada.',
+        })
+      }
+    })
+  })
 }
 
 export default function SearchPage() {
