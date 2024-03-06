bc95315ac4aa936c62b2bc020e041ea3a4395d43
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-03-07 00:38:41 +0100

    fix(orders): Order created-at

--- src/app/(pages)/carrinho/page.tsx
+++ src/app/(pages)/carrinho/page.tsx
@@ -224,7 +224,7 @@ export default function CartPage() {
         lastName: data.lastName,
         address: data.address,
         phone: data.phone,
-        createdAt: new Date(),
+        createdAt: new Date().toISOString(),
         products: productsList,
       })
         .then(() => {
