28de15a288c8d8323fa911758e688a383a8d77ef
Carlinhos <91576261+carl0smat3us@users.noreply.github.com>
2024-03-10 14:59:42 +0000

    fix: Recovery account

--- src/app/(pages)/recuperar-conta/page.tsx
+++ src/app/(pages)/recuperar-conta/page.tsx
@@ -5,9 +5,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import { Bounce, toast } from 'react-toastify'
 import Header from '@/app/components/Header'
-import FieldError from '@/app/components/FieldError'
 import { sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '@/lib/firebase/config'
+import Field from '@/app/components/Field'
 
 const schema = z.object({
   email: z.string().email('Preencha um e-mail válido'),
@@ -72,13 +72,13 @@ export default function RecoverAccount() {
           </h3>
           <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
             <div>
-              <label className="font-medium">E-mail</label>
-              <input
+              <Field.Label htmlFor="email">E-mail</Field.Label>
+              <Field.Input
                 type="email"
                 {...register('email')}
-                className={`w-full bg-neutral-100 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border ${errors.email && 'border-red-500'}`}
+                error={errors.email}
               />
-              <FieldError error={errors.email} />
+              <Field.Error error={errors.email} />
             </div>
 
             <button
