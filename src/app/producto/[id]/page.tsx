cae3b0228164d57dfd5a0786e3d3b3dbb4ba7246
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-07-05 22:52:08 +0200

    add space

--- src/app/producto/[id]/page.tsx
+++ src/app/producto/[id]/page.tsx
@@ -107,6 +107,7 @@ export default async function ProductPage({
                   )
                 : formatMoney(product.price)}
             </span>
+
             {product.campaign &&
               campaignValidator(product.campaign) ===
                 'campaign-with-promotion' && (
