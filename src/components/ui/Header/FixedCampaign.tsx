e649294a7a8bfb6dcb1197e239baa1ec0964a96a
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-07-05 22:42:50 +0200

    fix: FixedCampaign component invalid properties

--- src/components/ui/Header/FixedCampaign.tsx
+++ src/components/ui/Header/FixedCampaign.tsx
@@ -6,17 +6,20 @@ import { useInformation } from '@/hooks/useInformation'
 export default function FixedCampaign() {
   const { informationsData } = useInformation()
   const { campaignData } = useCampaign()
+  let campaign
+
+  if (campaignData && informationsData.fixedCampaign) {
+    campaign = campaignData[informationsData.fixedCampaign]
+  }
 
   return (
     <div>
-      {informationsData.fixedCampaign && (
+      {informationsData.fixedCampaign && campaign && (
         <Link
           href={`/campanha/${informationsData.fixedCampaign}`}
           className="inline-block w-full bg-secondary p-[6px]"
         >
-          <p className="text-center text-sm font-bold">
-            {campaignData[informationsData.fixedCampaign].title}
-          </p>
+          <p className="text-center text-sm font-bold">{campaign.title}</p>
         </Link>
       )}
     </div>
